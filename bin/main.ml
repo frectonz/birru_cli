15c9b13e4f9fdbc3ff9c0e96c617bd4a268ed892
frectonz <fraol0912@gmail.com>
2022-11-27 12:47:06 +0300

    color and refactoring

--- bin/main.ml
+++ bin/main.ml
@@ -17,6 +17,32 @@ let parse_currency json =
   let currency = json |> member "currency" |> to_string in
   {selling; buying; currency}
 
+let colored_string color string = ANSITerminal.sprintf [color] "%s" string
+
+let greenify = colored_string ANSITerminal.green
+
+let yellowify = colored_string ANSITerminal.yellow
+
+let blueify = colored_string ANSITerminal.blue
+
+let make_padding max current = String.make (max - String.length current) ' '
+
+let join sep lst = List.fold_right (fun acc e -> acc ^ sep ^ e) lst ""
+
+let print_currency
+    (max_currency_col_width, max_selling_col_width, max_buying_col_width)
+    {selling; buying; currency} =
+  let currency =
+    greenify currency ^ make_padding max_currency_col_width currency
+  in
+  let selling = string_of_float selling in
+  let selling =
+    make_padding max_selling_col_width selling ^ yellowify selling
+  in
+  let buying = string_of_float buying in
+  let buying = make_padding max_buying_col_width buying ^ blueify buying in
+  [currency; selling; buying] |> join "\t" |> print_endline
+
 let () =
   let body = Lwt_main.run body in
   let body = Yojson.Basic.Util.to_list body in
@@ -27,13 +53,13 @@ let () =
       (fun acc currency -> max acc (String.length currency.currency))
       0 body
   in
-  let _ =
+  let max_selling_col_width =
     List.fold_left
       (fun acc currency ->
         max acc (String.length (string_of_float currency.selling)) )
       0 body
   in
-  let _ =
+  let max_buying_col_width =
     List.fold_left
       (fun acc currency ->
         max acc (String.length (string_of_float currency.buying)) )
@@ -42,36 +68,18 @@ let () =
   let currency = "Currency" in
   let selling = "Selling" in
   let buying = "Buying" in
-  let currency_padding =
-    String.make (max_currency_col_width - String.length currency) ' '
-  in
-  let selling_padding =
-    String.make (max_currency_col_width - String.length selling) ' '
-  in
-  let buying_padding =
-    String.make (max_currency_col_width - String.length buying) ' '
-  in
-  let header = 
-      ( "| "  ^ currency ^ currency_padding ^ " | " ^ selling_padding ^ selling
-      ^ " | " ^ buying_padding ^ buying ^ " | " )
+  let currency_padding = make_padding max_currency_col_width currency in
+  let selling_padding = make_padding max_selling_col_width selling in
+  let buying_padding = make_padding max_buying_col_width buying in
+  let header =
+    [ greenify currency ^ currency_padding
+    ; selling_padding ^ yellowify selling
+    ; buying_padding ^ blueify buying ]
+    |> join "\t"
   in
   let () = print_endline header in
-  let () = print_endline (String.make (String.length header) '-') in
+  let () = print_endline "" in
   body
-  |> List.iter (fun {selling; buying; currency} ->
-         let padding =
-           String.make (max_currency_col_width - String.length currency) ' '
-         in
-         let currency = "| " ^ currency ^ padding ^ " | " in
-         let selling = string_of_float selling in
-         let padding =
-           String.make (max_currency_col_width - String.length selling) ' '
-         in
-         let selling = padding ^ selling ^ " | " in
-         let buying = string_of_float buying in
-         let padding =
-           String.make (max_currency_col_width - String.length buying) ' '
-         in
-         let buying = padding ^ buying ^ " |" in
-         let row = currency ^ selling ^ buying in
-         print_endline row )
+  |> List.iter
+       (print_currency
+          (max_currency_col_width, max_selling_col_width, max_buying_col_width) )
